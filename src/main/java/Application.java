6ee6028809dfe34778b9730d9f324ed516ecdb9b
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-08-20 23:13:48 +0300

    added support to multi-file upload
    removed name field and added picture name as name

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,18 +4,24 @@ import config.AppDataSource;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
+import org.eclipse.jetty.util.MultiPartInputStreamParser;
 import spark.*;
 import spark.utils.IOUtils;
 
 import javax.imageio.ImageIO;
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletException;
 import javax.servlet.http.Part;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static spark.SparkBase.staticFileLocation;
 
@@ -33,15 +39,7 @@ public class Application {
         Spark.post("/add", (request, response) -> {
             setRequestMultiPartFile(request);
 
-            Part file = request.raw().getPart("file");
-            Part name = request.raw().getPart("name");
-            String nameStr = IOUtils.toString(name.getInputStream());
-
-            try (Connection connection = AppDataSource.getTransactConnection();
-                 PreparedStatement ps = connection.prepareStatement("INSERT INTO FILE(name, content, image_width, image_height) VALUES (?, ?, ?, ?)")) {
-                fillPreparedStatement(ps, file, nameStr);
-                connection.commit();
-            }
+            saveFilesToDb(request);
             response.redirect("/");
             return "success";
         }, toJson);
@@ -87,16 +85,54 @@ public class Application {
 
     }
 
+    private static void saveFilesToDb(Request request) throws IOException, ServletException, SQLException {
+        List<Part> parts = getParts(request);
+        if (parts.size() == 1) {
+            saveOneFile(parts.get(0));
+        } else {
+            for (Part file : parts) saveOneFile(file);
+        }
+    }
+
+    /**
+     * It is magical, I cast to List, (it's ArrayList),
+     * all objects are <Part> yet cannot cast at the spot
+     */
+    private static List<Part> getParts(Request request) throws IOException, ServletException {
+        return (List) request.raw().getParts();
+    }
+
+    private static void saveOneFile(Part file) throws IOException, ServletException, SQLException {
+        try (Connection connection = AppDataSource.getTransactConnection();
+             PreparedStatement ps = connection.prepareStatement("INSERT INTO FILE(name, content, image_width, image_height) VALUES (?, ?, ?, ?)")) {
+            fillPreparedStatement(ps, file);
+            connection.commit();
+        }
+    }
+
     private static void setRequestMultiPartFile(Request request) {
         request.raw().setAttribute("org.eclipse.multipartConfig", new MultipartConfigElement("/tmp"));
     }
 
-    private static void fillPreparedStatement(PreparedStatement ps, Part file, String name) throws IOException, SQLException {
-        BufferedImage img = ImageIO.read(file.getInputStream());
-        ps.setString(1, name);
+    private static void fillPreparedStatement(PreparedStatement ps, Part file) throws IOException, SQLException {
+        ps.setString(1, getFileName(file));
         ps.setBinaryStream(2, file.getInputStream(), (int) file.getSize());
+        BufferedImage img = ImageIO.read(file.getInputStream());
         ps.setInt(3, img.getWidth());
         ps.setInt(4, img.getHeight());
         ps.executeUpdate();
+    }
+
+    /**
+     * hack from the web
+     */
+    static private String getFileName(Part part) {
+        for (String cd : part.getHeader("content-disposition").split(";")) {
+            if (cd.trim().startsWith("filename")) {
+                return cd.substring(cd.indexOf('=') + 1).trim()
+                        .replace("\"", "");
+            }
+        }
+        return "unknown";
     }
 }
