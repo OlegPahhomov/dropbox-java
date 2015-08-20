9c54e06699b9dd5c445e429397558ac40ee57e15
oleg.pahhomov <oleg.pahhomov@gmail.com>
2015-08-20 12:18:34 +0300

    refactored css, html and js, added gitignore

--- src/main/java/config/AppDataSource.java
+++ src/main/java/config/AppDataSource.java
@@ -6,15 +6,17 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class AppDataSource {
-
     private static PGPoolingDataSource dataSource;
+    public static final String HOME_PW = "12345";
+    public static final String OFFICE_PW = "postgres";
 
     static {
         // Not recommended, you may need to implement connection pooling or use apache database pooling lib
         dataSource = new PGPoolingDataSource();
         dataSource.setDatabaseName("postgres");
         dataSource.setUser("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setPassword(OFFICE_PW);
+        dataSource.setPassword(HOME_PW);
         dataSource.setServerName("localhost");
     }
 
---
+++ src/main/resources/webapp/js/load-files.js
@@ -0,0 +1,22 @@
+var fileLoader = {
+
+    load: function loadFiles() {
+        var showFilesElem = $("#show_files");
+        $.ajax("http://localhost:4567/files")
+            .done(function (response) {
+                showFilesElem.html(tmpl("show_files_template",
+                    {
+                        files: response,
+                        getRatioClass: function (image) {
+                            var golden_ratio = 1.45;
+                            return image.ratio > golden_ratio ? 'file bigfile' : 'file';
+                        }
+                    }
+                ))
+            }).fail(function (jqXHR, textStatus) {
+                showFilesElem.html("Error occurred");
+                console.log("Request failed: " + textStatus);
+            });
+
+    }
+}
